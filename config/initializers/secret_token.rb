fde60bec66c8b1692457a114cf1c995732a6a0aa
Andy Brett <andy@andybrett.com>
2010-10-30 19:48:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hunchcrunch::Application.config.secret_token = 'd4636d524527bb6b0de51d15625a19d45ec86d139ec34a7c5621f523b9527927ed2124538be280dcab7e922d289673d4e972ff803a4bb3a1b8f9638f202afd56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hunchcrunch::Application.config.session_store :cookie_store, :key => '_hunchcrunch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Hunchcrunch::Application.config.session_store :active_record_store
